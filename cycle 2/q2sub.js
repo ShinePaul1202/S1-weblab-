b2514b766e952821ecf47f42232f311dc50a24de
ShinePaul1202 <24mp2316@rit.ac.in>
2024-10-23 23:00:31 +0530

    Delete cycle 2 directory

--- cycle 2/q2sub.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('onButton').addEventListener('click', function() {
-    document.getElementById('lightBulb').src = 'bulbon.png'; 
-});
-
-document.getElementById('offButton').addEventListener('click', function() {
-    document.getElementById('lightBulb').src = 'bulboff.png'; 
-});
-
