395443283077f34b1350b4db80ee92142eb20fca
ShinePaul1202 <24mp2316@rit.ac.in>
2024-10-23 23:01:17 +0530

    Add files via upload

---
+++ cycle 3/q2sub.js
@@ -0,0 +1,8 @@
+document.getElementById('onButton').addEventListener('click', function() {
+    document.getElementById('lightBulb').src = 'bulbon.png'; 
+});
+
+document.getElementById('offButton').addEventListener('click', function() {
+    document.getElementById('lightBulb').src = 'bulboff.png'; 
+});
+
