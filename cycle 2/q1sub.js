b73a7bc77cb342b93ef5f4a248292d15c9d89847
ShinePaul1202 <24mp2316@rit.ac.in>
2024-10-23 22:59:30 +0530

    Add files via upload

---
+++ cycle 2/q1sub.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("colorButton");
+
+button.addEventListener('click', function() {
+    document.body.style.backgroundColor = "red";
+});
+
+button.addEventListener('dblclick', function() {
+    document.body.style.backgroundColor = "green";
+});
+
