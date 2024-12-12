b7b6f84598e2480f395bde933169a21499ad50cc
starzz <24mp2316@rit.ac.in>
2024-12-12 11:54:56 +0530

    Delete cycle4 directory

--- cycle4/pg1/student.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$id=$_POST['id'];
-	$name=$_POST['name'];
-	$age=$_POST['age'];
-	$class=$_POST['class'];
-	echo "Name of student:$name<br> Id:$id <br> Age:$age <br> class:$class";
-?>
--- cycle4/pg3/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Clear cookies
-setcookie('student_id', '', time() - 3600, "/");
-setcookie('student_email', '', time() - 3600, "/");
-
-header('Location: login.php');  // Redirect to login page
-exit();
-?>
-
